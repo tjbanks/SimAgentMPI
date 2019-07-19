a492cf1ae38245aefab852dca1e5de08ce675b19
Tyler Banks <34382569+tjbanks@users.noreply.github.com>
2019-07-19 16:29:26 -0500

    Update analysis.py

--- SimAgentMPI/Models/BLA/analysis.py
+++ SimAgentMPI/Models/BLA/analysis.py
@@ -1,14 +1,34 @@
 # Python
+#import matplotlib.pyplot as plt
+#import pandas as pd
+#import sys
+
+#df = pd.read_csv('data',sep='\t',header=None)
+#spiketimes = df[0].tolist()
+
+#bins = 500
+#if len(sys.argv) == 2:
+#    bins = int(sys.argv[1])
+	
+#plt.hist(spiketimes,normed=False,bins=bins)
+#plt.show()
+
+# Python
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
 df = pd.read_csv('data',sep='\t',header=None)
+df = df.loc[(df[1] >= 900)]
+print(df)
 spiketimes = df[0].tolist()
+cells = df[1].tolist()
 
 bins = 500
 if len(sys.argv) == 2:
     bins = int(sys.argv[1])
 	
-plt.hist(spiketimes,normed=False,bins=bins)
+#plt.hist(spiketimes,normed=False,bins=bins)
+plt.scatter(spiketimes,cells,s=1)
+plt.xlim(45000,45500)
 plt.show()
